8b5a038d92918d1035035fc8881c63e4175369ec
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-06-17 13:58:29 +0530

    Update jwt-controller.js

--- controller/jwt-controller.js
+++ controller/jwt-controller.js
@@ -4,12 +4,14 @@ dotenv.config();
 export const authenticateToken = async (request, response, next) => {
   const authHeader = request.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) {
-    return response.status(401).json({ msg: "token is missing" });
+  if (token === null || token === undefined) {
+    return response
+      .status(401)
+      .json({ msg: "token is missing", clearToken: true });
   }
   jwt.verify(token, process.env.ACCESS_SECRET_KEY, (error, user) => {
     if (error) {
-      return response.status(404).json({ msg: "Invalid Token" });
+      return response.status(404).json({ msg: "Invalid Token", clearToken: true });
     }
     request.user = user;
     next();
