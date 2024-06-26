6471db4f70c487fc45f8747b6de9de2d0299a755
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-06-26 12:48:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = 8000;
 app.use(cors());
 app.use(bodyParser.json({ extended: "true" }));
 app.use(bodyParser.urlencoded({ extended: "true" }));
+app.use(bodyParser.text({ extended: "true" }));
 app.use("/", router);
 
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
