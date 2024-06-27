2c131efa6d76c6b4043f502625936d679a2f8007
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-06-27 17:46:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 dotenv.config();
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors({
   origin: '*',
