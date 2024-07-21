3ab53876019fa4ed13dad7334a06a6b5352ed6ed
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-07-21 21:04:30 +0530

    Update route.js

--- router/route.js
+++ router/route.js
@@ -21,7 +21,7 @@ router.post("/login", loginUser);
 router.post("/file/upload", upload.single("file"), uploadImage);
 router.get("/file/:filename", getImage);
 router.post("/create",authenticateToken, createPost);
-router.get("/posts",authenticateToken, getAllPosts);
+router.get("/posts", getAllPosts);
 router.get("/post/:id",authenticateToken, getPost);
 router.put("/update/:id",authenticateToken, updatePost);
 router.delete("/deletePost/:id",authenticateToken, deletePost);
